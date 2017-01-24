917c98cbb156ef71e7a3178b9cc6db0ca5bdb17a
Vladimir Kompaniec <vkompaniec@gmail.com>
2017-01-24 15:56:54 +0300

    Инициализирующий коммит

--- server.py
+++ server.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+import json
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def form():
     return render_template('form.html')
+
+@app.route('/typograf',methods=['POST'])
+def typograf():
+        result = ""
+        for character in request.form['text']:
+            result += str(ord(character))+ " "
+        return jsonify({'text':result})
 
 if __name__ == "__main__":
     app.run()
